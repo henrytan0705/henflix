a0d61229c41a561ede9aaed90ac45cf16a1168b9
HenryTan0705 <htan0705@gmail.com>
2019-07-30 23:06:22 -0400

    testing

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import signup_form_container from './session/signup_form_container';
 import login_form_container from './session/login_form_container';
-import navbar_container from './navbar/navbar_container.js';
+// import navbar_container from './navbar/navbar_container.js';
 
 import { Route } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ const App = () => {
     return (
         <div>
            <h1>HENFLIX IS LIVE!!</h1>
-
+            <a href="https://www.netflix.com/browse">HENFLIX</a>
             <Route path="" component={signup_form_container}/>
             <Route path="" component={login_form_container}/>
         </div>
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/root_reducer';
-// import { logger } from 'redux-logger';
+import { logger } from 'redux-logger';
 import thunk from '../middlewares/thunk';
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
 }
 
 export default configureStore;

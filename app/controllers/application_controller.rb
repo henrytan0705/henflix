d4a939ed8e365b51926ae83d7af3b2daf18f0827
HenryTan0705 <htan0705@gmail.com>
2019-07-31 09:19:25 -0400

    Controller helper method typo correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     #HCELL
 
-    helper_method :current_user, :loggined_in?
+    helper_method :current_user, :logged_in?
 
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])

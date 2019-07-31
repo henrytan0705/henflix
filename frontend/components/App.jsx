5c267db92ffa57f08cf65efc16ce20080fd27ab1
HenryTan0705 <htan0705@gmail.com>
2019-07-31 14:22:51 -0400

    Add css reset file

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import SignupFormContainer from './session/signup_form_container';
 import LoginFormContainer from './session/login_form_container';
 import NavbarContainer from './navbar/navbar_container.js';
-
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { AuthRoute } from '../utils/route_util';
 
 const App = () => {
     return (
-        <div>
-           <h1>HENFLIX IS LIVE!!</h1>
-            {/* <Switch> */}
+        <div className="splash">
+            <div className="">
+                <h1>HENFLIX IS LIVE!!</h1>
                 <Route path="/" component={NavbarContainer}/>
                 <AuthRoute exact path="/signup" component={SignupFormContainer}/>
                 <AuthRoute exact path="/login" component={LoginFormContainer}/>
+            </div>
                 {/* <Route path="/browse" component=""/> */}
-            {/* </Switch> */}
         </div>
     )
 };

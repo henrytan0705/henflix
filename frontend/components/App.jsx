bc5df7c6e45896d87e5613837d332e8e537b24b3
HenryTan0705 <htan0705@gmail.com>
2019-08-03 17:21:06 -0400

    test

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,12 +3,13 @@ import SignupFormContainer from './session/signup_form_container';
 import LoginFormContainer from './session/login_form_container';
 // import NavbarContainer from './navbar/navbar_container';
 import SplashContainer from './splash/splash_container';
-import MainContainer from './main/main_container';
-// import Main from './main/main';
+// import MainContainer from './main/main_container';
+import Main from './main/main';
 // import Footer from './footer/footer';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import { AuthRoute } from '../utils/route_util';
 import { MainAuthRoute } from '../utils/main_route_util';
+
 const App = () => {
     return (
         <>
@@ -16,8 +17,8 @@ const App = () => {
             <AuthRoute exact path="/" component={SplashContainer}/>
             <AuthRoute exact path="/signup" component={SignupFormContainer}/>
             <AuthRoute exact path="/login" component={LoginFormContainer}/>
-            <MainAuthRoute path="/browse" component={MainContainer}/>
-            {/* <Route exact path="/browse" component={Main}/> */}
+            {/* <MainAuthRoute path="/browse" component={MainContainer}/> */}
+            <MainAuthRoute path="/browse" component={Main}/>
             {/* <Footer /> */}
         </>
     )

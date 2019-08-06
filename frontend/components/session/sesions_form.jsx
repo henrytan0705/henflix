27d3e1565152c5f6cd2a35bf7a9f33fa020aaa87
HenryTan0705 <htan0705@gmail.com>
2019-08-06 02:21:43 -0400

    Restyle form page

--- frontend/components/session/sesions_form.jsx
+++ frontend/components/session/sesions_form.jsx
@@ -52,27 +52,30 @@ class SessionForm extends React.Component {
             
         return(
             <>
-            <div>
+            {/* <div> */}
+            
                 <NavbarFormContainer/>
-                <div className="form-page">
+                {/* <div className="form-page">
                     <div className="session-form">
                         <form className="form" onSubmit={this.handleSubmit}>
 
                             <h2 className="formtype">{this.props.formType}</h2>
-                        
-                            <input className="email" type="text" value={this.state.email} onChange={this.update("email")} placeholder="Email or phone number"/>
-                            <div className="errors">{this.props.errors[0]}</div>
 
-                            <br/>
+                            <div>
+                                <input className="email" type="text" value={this.state.email} onChange={this.update("email")} placeholder="Email or phone number"/>
+                                <div className="errors">{this.props.errors[0]}</div>
+                            </div>
+                           
 
-                            <input className="password" type="password" value={this.state.password} onChange={this.update("password")} placeholder="Password"/>
-                            <div className="errors">{this.props.errors[1]}</div>
-
-                            <br/>
+                            <div>
+                                <input className="password" type="password" value={this.state.password} onChange={this.update("password")} placeholder="Password"/>
+                                <div className="errors">{this.props.errors[1]}</div>
+                            </div>
+                           
                             <input className="submit-button" type="submit" value={this.props.formType}/>
                         
                             {demo}
-                            <br/>
+                            
                             {linkText}
 
                             <Link className="link-button" 
@@ -80,10 +83,67 @@ class SessionForm extends React.Component {
                                 onClick={this.props.clearErrors}>
                                     {linkName}
                             </Link>
+
                         </form>
                     </div>
                 </div>
+            </div> */}
+        <div className="form-background">
+            <div className="form-body">
+                <div>
+                    <div className="form-content">
+                        <div className="form-main"> 
+                          
+                            <h1 className="formtype">{this.props.formType}</h1>
+
+                            <form className="form" onSubmit={this.handleSubmit}>
+
+                                <div className="form-email-input">
+                                    <div className="input">
+                                        <div className="email-label-wrapper">
+                                            <label> 
+                                                <input id="email" className="email-input" type="text" value={this.state.email} onChange={this.update("email")} placeholder="Email or phone number"/>
+                                                <div className="errors">{this.props.errors[0]}</div>
+                                                {/* <label for="email" className="email-label">Email or phone number</label> */}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="form-password-input">
+                                    <div className="input">
+                                        <div className="password-label-wrapper">
+                                            <label>
+                                                {/* <label className="password-label"> Password */}
+                                                    <input className="password-input" type="password" value={this.state.password} onChange={this.update("password")} placeholder="Password"/>
+                                                    <div className="errors">{this.props.errors[1]}</div>
+                                                {/* </label> */}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button className="form-button" type="submit">{this.props.formType}</button>
+
+                                {demo}
+
+                                <span className="link-text">{linkText}
+
+                                    <Link className="link-button"
+                                        to={formlink}
+                                        onClick={this.props.clearErrors}>
+                                        {linkName}
+                                    </Link>
+
+                                </span>
+                            </form>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
+
+                </div>
             </>
         )
     }

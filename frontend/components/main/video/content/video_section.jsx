336723b27208a34ba0d642e55ff995f5ecc48010
HenryTan0705 <htan0705@gmail.com>
2019-08-26 15:54:04 -0400

    remove categories if videos do not exist for category

--- frontend/components/main/video/content/video_section.jsx
+++ frontend/components/main/video/content/video_section.jsx
@@ -2,43 +2,82 @@ import React from 'react'
 import VideoIndexContainer from './video_index_container'
 
 class VideoSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mounted: true
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+
     render() {
         let videosArray = Object.values(this.props.videos);
         // debugger
         
-        let genres = Object.values(this.props.genres).filter(genre => genre.genre != "TV Show" && genre.genre != "Movie");
+        // let genres = Object.values(this.props.genres).filter(genre => genre.genre != "TV Show" && genre.genre != "Movie");
 
         // let animation = genres.
-
+        
         let animationVideos = videosArray.filter(video => video.genres.includes("Animation")); 
- 
+        let animationRow = (!Object.values(animationVideos).length) ? null : (
+            <>
+                <h1 className="categories">Animations</h1>
+                <VideoIndexContainer genre="Animation" videos={animationVideos} />
+            </>
+        )
+        
+        // debugger
         let foodVideos = videosArray.filter(video => video.genres.includes("Food"));
-        
+        let foodRow = (!Object.values(foodVideos).length) ? null : (
+            <>
+                <h1 className="categories">Food/Horror</h1>
+                <VideoIndexContainer genre="Food" videos={foodVideos} />
+            </>
+        )
+        // debugger
         let liveActionVideos = videosArray.filter(video => video.genres.includes("Live Action"));
-        
-        let educationalVideos = videosArray.filter(video => video.genres.includes("Educational"));
+        let liveRow = (!Object.values(liveActionVideos).length) ? null : (
+            <>
+                <h1 className="categories">Live Action</h1>
+                <VideoIndexContainer genre="Live Action" videos={liveActionVideos} />  
+            </>
+        )
 
+        let educationalVideos = videosArray.filter(video => video.genres.includes("Educational"));
+        let educationalRow = (!Object.values(educationalVideos).length) ? null : (
+            <>
+                <h1 className="categories">Educational</h1>
+                <VideoIndexContainer genre="Educational" videos={educationalVideos} />
+            </>
+        )
+        
         return (
             <div className="video-section">
                 {/* {/* <h1 className="categories">Trending Now</h1> */}
                 {/* <VideoIndexContainer genre="Trending" videos={trendingVideos}/> */}
                 
-                <h1 className="categories">Animations</h1>
-                <VideoIndexContainer genre="Animation" videos={animationVideos}/>  
-                {/* <VideoIndexContainer genre="Animation" />   */}
+                {/* <h1 className="categories">Animations</h1>
+                <VideoIndexContainer genre="Animation" videos={animationVideos}/>   */}
+           
+                {animationRow}
 
-                <h1 className="categories">Food/Horror</h1>
-                <VideoIndexContainer genre="Food" videos={foodVideos}/>
-                {/* <VideoIndexContainer genre="Food" /> */}
+                {/* <h1 className="categories">Food/Horror</h1>
+                <VideoIndexContainer genre="Food" videos={foodVideos}/> */}
+             
+                {foodRow}
                 
-                <h1 className="categories">Live Action</h1>
-                <VideoIndexContainer genre="Live Action" videos={liveActionVideos}/>  
-                {/* <VideoIndexContainer genre="Live Action" />   */}
+                {/* <h1 className="categories">Live Action</h1>
+                <VideoIndexContainer genre="Live Action" videos={liveActionVideos}/>   */}
 
-                <h1 className="categories">Educational</h1>
-                <VideoIndexContainer genre="Educational" videos={educationalVideos}/>
-                {/* <VideoIndexContainer genre="Educational"/> */}
+                {liveRow}
 
+                {/* <h1 className="categories">Educational</h1>
+                <VideoIndexContainer genre="Educational" videos={educationalVideos}/> */}
+
+                {educationalRow}
             </div>
         )
     }

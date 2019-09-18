e94a65d2c82e2c3059ebbccabb3202b51996bdf9
HenryTan0705 <htan0705@gmail.com>
2019-09-17 21:02:56 -0400

    modify search results render within search component

--- frontend/components/main/search.jsx
+++ frontend/components/main/search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import NavbarMainContainer from '../navbar/navbar_main_container';
-import SearchVideoIndexContainer from './video/content/search_video_index_container';
+import VideoIndexContainer from './video/content/search_video_index_container';
 
 class Search extends React.Component {
     componentDidMount() {
@@ -12,12 +12,29 @@ class Search extends React.Component {
     }
 
     render() {
-        let content = (Object.keys(this.props.searchResults).length > 0) ?
-            (<SearchVideoIndexContainer videos={this.props.searchResults} />) 
-                :
-            (<div className="no-search-result">
-                <h1> 
-                    Your search for {this.props.searchStatus.query} did not have any matches.
+        if (Object.keys(this.props.searchResults).length > 0) {
+            let videoItems = [];
+            let arr = [];
+            let videos = Object.values(this.props.searchResults);
+    
+            for (let i = 0; i < videos.length; i++) {
+                arr.push(videos[i])
+    
+                if (arr.length === 6 || i === videos.length - 1) {
+                    videoItems.push(arr);
+                    arr = []; 
+                }
+            }
+    
+            this.content = videoItems.map((row) => {
+                return (
+                    <VideoIndexContainer videos={row}/>
+                )
+            })
+        } else {
+            this.content = ((<div className="no-search-result">
+                <h1>
+                    Your search for "{this.props.searchStatus.query}" did not have any matches.
                 </h1>
                 <p>Suggestions:</p>
                 <ul>
@@ -28,24 +45,20 @@ class Search extends React.Component {
                 </ul>
             </div>
             )
+            )
+        }
 
-        // if (Object.keys(this.props.searchResults).length > 0) {
-        //     content = (
-        //         <VideoIndexContainer videos={this.props.searchResults} />
-        //     )
-        // } else {
-        //     <h1>
-        //         No results matching search.
-        //     </h1>
-        // }
+
+
+        // let content = (Object.keys(this.props.searchResults).length > 0) ?
+            // (<SearchVideoIndexContainer videos={this.props.searchResults} />) 
+                // :
+            
+
 
         return (
             <div className="search-page">
-                {/* <NavbarMainContainer/> */}
-                {/* <div className='main-content'> */}
-                    {/* <VideoIndexContainer videos={this.props.searchResults}/> */}
-                {content}
-                {/* </div> */}
+                {this.content}
             </div>
         )
     }

5d4547fbe27decffea5219015d29701bc9671120
HenryTan0705 <htan0705@gmail.com>
2019-09-21 18:58:00 -0400

    remove debugger for live

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -7,7 +7,7 @@ class Api::ListsController < ApplicationController
     def show #show videos in list
         @videos = User.where(id: current_user.id)[0].list_shows.with_attached_photo.with_attached_video_url
         render :show
-    end
+    end 
 
     def create #add video to list
         
--- app/views/api/videos/index.json.jbuilder
+++ app/views/api/videos/index.json.jbuilder
@@ -1,4 +1,3 @@
-debugger
 @videos.each do |video|
     json.set! video.id do
         json.extract! video, :id, :title, :description, :year
